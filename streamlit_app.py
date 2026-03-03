71340b46aa0a89c3d35d2433863a367ce491762d
Melvyn Shibu George <melvynshibu@gmail.com>
2026-03-03 23:23:27 +0530

    Switch to displaying pandas dataframe instead of Snowpark
    Comment out the original dataframe display and convert to pandas dataframe for display.

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,12 @@ cnx= st.connection("snowflake")
 session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+
+#Convert the Snowpark Dataframe to pandas Dataframe so we can use the LOC Function 
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 ingredients_list = st.multiselect(
