f39fc10132cdceb52cc23011a7a6bf5ec8daddb7
Melvyn Shibu George <melvynshibu@gmail.com>
2026-03-04 10:25:51 +0530

    Comment out order insertion logic
    Comment out SQL insert statement and related code.

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,9 +48,9 @@ if ingredients_list:
         smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")  
         sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
         
-        my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-            values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
-    st.write(my_insert_stmt)
+       # my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+        #    values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
+    #st.write(my_insert_stmt)
     #st.stop()
 
   
