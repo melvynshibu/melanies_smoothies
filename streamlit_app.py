dbb170cfcaaea9827ca059a6ee377502c740b334
Melvyn Shibu George <melvynshibu@gmail.com>
2026-03-10 18:54:52 +0530

    Update success message for smoothie order

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,7 +58,7 @@ if ingredients_list:
     time_to_insert = st.button('Submit Order')
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
-        st.success('Your Smoothie is ordered {name_on_order}', icon="✅")
+        st.success('Your Smoothie is ordered Betty Jean!', icon="✅")
 
 
  
